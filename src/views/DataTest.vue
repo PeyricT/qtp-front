b32f81cd052d936c9727037b29e79b48a21f5000
heboquet <heboquet2@free.fr>
2023-03-21 14:34:43 +0100

    tab test

---
+++ src/views/DataTest.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="root">
+   <p class="text-xl font-medium m-2"> TEST </p>
+   <Button>
+        Test1
+    </Button>
+<Button>
+        Test2
+    </Button>
+<Button>
+        Test3
+    </Button>
+<Button>
+        Test4
+    </Button>
+</template>
