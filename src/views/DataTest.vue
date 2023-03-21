561864afec036a9ad4aa518fbae746189490452d
Thibaut Peyric <thibaut.peyric@proton.me>
2023-03-21 15:25:40 +0100

    repare DataTest

--- src/views/DataTest.vue
+++ src/views/DataTest.vue
@@ -1,18 +1,49 @@
 <template>
-POUET
+<ul>
+<li>
    <Button>
         Test1
     </Button>
-<Button>
+</li>
+<li>
+   <Button>
         Test2
     </Button>
-<Button>
+</li>
+<li>
+   <Button>
         Test3
     </Button>
-<Button>
+</li>
+<li>
+   <Button>
         Test4
     </Button>
+</li>
+<li>
+   <Button>
+        Test5
+    </Button>
+</li>
+<li>
+   <Button>
+        Test6
+    </Button>
+</li>
+</ul>
+
+<p> here are printed results </p>   
 </template>
 
 <script lang="ts">
 import Button from 'primevue/button'
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'DataTest',
+  components: {
+  },
+  mounted(){
+    //console.log("Mounting DataTest");
+  }
+});
+</script>
